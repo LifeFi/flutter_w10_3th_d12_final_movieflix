6f1e080870b9e820671f326b00f59cade03f4f1d
topcircle <topcircle@gmail.com>
2024-06-02 19:56:24 +0900

    fvm 설정

--- lib/views/components/movie_list.dart
+++ lib/views/components/movie_list.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_w10_3th_d12_final_movieflix/models/movie_model.dart';
+import 'package:flutter_w10_3th_d12_final_movieflix/views/components/interactive_container.dart';
 import 'package:go_router/go_router.dart';
 
 enum MovieListType { large, medium }
@@ -40,37 +41,41 @@ class MovieList extends StatelessWidget {
             itemCount: movies.length,
             itemBuilder: (context, index) {
               final movie = movies[index];
-              return GestureDetector(
-                onTap: () => _goToMovie(movie.id.toString(), context),
-                child: SizedBox(
-                  width: type == MovieListType.large ? 340 : 165,
-                  child: Column(
-                    crossAxisAlignment: CrossAxisAlignment.start,
-                    children: [
-                      Container(
-                        clipBehavior: Clip.hardEdge,
-                        decoration: BoxDecoration(
-                          borderRadius: BorderRadius.circular(10),
-                        ),
-                        child: Image.network(
-                          movie.thumb,
-                          width: double.maxFinite,
-                          height: type == MovieListType.large ? 250 : 180,
-                          fit: BoxFit.cover,
-                        ),
-                      ),
-                      if (type != MovieListType.large) ...[
-                        const SizedBox(height: 10),
-                        Text(
-                          movie.title,
-                          style: const TextStyle(
-                            fontWeight: FontWeight.w600,
+              return InteractiveContainer(
+                bottomValue: 0.95,
+                topValue: 1.05,
+                child: GestureDetector(
+                  onTap: () => _goToMovie(movie.id.toString(), context),
+                  child: SizedBox(
+                    width: type == MovieListType.large ? 340 : 165,
+                    child: Column(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: [
+                        Container(
+                          clipBehavior: Clip.hardEdge,
+                          decoration: BoxDecoration(
+                            borderRadius: BorderRadius.circular(10),
                           ),
-                          maxLines: 3,
-                          overflow: TextOverflow.ellipsis,
+                          child: Image.network(
+                            movie.thumb,
+                            width: double.maxFinite,
+                            height: type == MovieListType.large ? 250 : 180,
+                            fit: BoxFit.cover,
+                          ),
                         ),
-                      ]
-                    ],
+                        if (type != MovieListType.large) ...[
+                          const SizedBox(height: 10),
+                          Text(
+                            movie.title,
+                            style: const TextStyle(
+                              fontWeight: FontWeight.w600,
+                            ),
+                            maxLines: 3,
+                            overflow: TextOverflow.ellipsis,
+                          ),
+                        ]
+                      ],
+                    ),
                   ),
                 ),
               );
